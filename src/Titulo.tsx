78ead2658c0724909a7784b41fa0e55ada7fdc8e
MrBexen <benjadiaz553@gmail.com>
2026-04-30 11:53:30 -0400

    se agrega el Proyecto

---
+++ src/Titulo.tsx
@@ -0,0 +1,13 @@
+function Titulo(){
+
+  //jsx -> React.createElement
+  const nombre = "Benja";
+  if (nombre){
+    return <h1>Hola {nombre}</h1>
+  }
+
+  return <h1>Hola Mundo</h1>
+
+}
+
+export default Titulo;
