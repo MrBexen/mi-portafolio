fb6b62fccd0c3ea23e3babdfd587c98b5468bd0a
MrBexen <benjadiaz553@gmail.com>
2026-08-20 12:31:41 -0400

    feat: se añadio el concepto de funciones como props

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,12 +1,20 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
-
+//Componente raiz
 function App() {
   const list = ["Gundam", "Guncannon", "Guntank"];
+  const handleSelect = (element: string) => {
+    console.log("imprimiendo ", element);
+  };
+
+  const handleSelect2 = (element: string) => {
+    console.log("mostrando ", element);
+  };
   return (
     <Card>
       <CardBody title="Hola mundo" text="Este es el texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
--- react_app/src/components/List.tsx
+++ react_app/src/components/List.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 type Props = {
   data: string[];
+  onSelect?: (elemet: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
+  //Hook (State Hook)
+  const [index, setIndex] = useState(1);
+  const handleClick = (i: number, element: string) => {
+    setIndex(i);
+    onSelect?.(element);
+  };
   return (
     <ul className="list-group">
-      {data.map((element) => (
-        <li key={element} className="list-group-item">
+      {data.map((element, i) => (
+        <li
+          onClick={() => handleClick(i, element)}
+          key={element}
+          className={`list-group-item ${index == i ? "active" : ""}`}
+        >
           {element}
         </li>
       ))}
