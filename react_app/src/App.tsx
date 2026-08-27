12412d92d4ed6de88fb67aec82934a6617defebb
MrBexen <benjadiaz553@gmail.com>
2026-08-27 15:26:50 -0400

    feat:Botton de cargando

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,9 +1,14 @@
 import Card, { CardBody } from "./components/Card";
+import Button from "./components/Button";
 import List from "./components/List";
+import { useState } from "react";
 //Componente raiz
 
 function App() {
-  const list: string[] = [];
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
+  const list: string[] = ["Gundam", "Guncannon", "Guntank"];
   const handleSelect = (element: string) => {
     console.log("imprimiendo ", element);
   };
@@ -22,6 +27,9 @@ function App() {
       ) : (
         "No hay contenido"
       )}
+      <Button isLoading={isLoading} onClick={handleClick}>
+        Hola Mundo
+      </Button>
     </Card>
   );
 }
---
+++ react_app/src/components/Button.tsx
@@ -0,0 +1,24 @@
+//tsrfce = Typescript React Functional Component Export
+
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  isLoading: boolean;
+  onClick: () => void;
+};
+
+function Button({ children, isLoading, onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={isLoading}
+      type="button"
+      className={`btn btn-${isLoading ? "secondary" : "primary"}`}
+    >
+      {isLoading ? "cargando..." : children}
+    </button>
+  );
+}
+
+export default Button;
